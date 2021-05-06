ffa291d368169adebf0f9ae1972ccb1b2c411334
DaniSan0508-projects <danielsantoscarneiro0508@gmail.com>
2021-05-06 01:04:23 -0300

    adicionado frases da sorte e classes para estilização

--- App.js
+++ App.js
@@ -4,15 +4,30 @@ class App extends Component{
 constructor(props){
     super(props)
     this.state = {}
+
+    this.frases = ['Siga os bons e aprenda com eles.','O riso é a menor distância entre duas pessoas.',
+    'Deixe de lado as preocupações e seja feliz','Acredite em milagres mais não dependa deles']
 }
 
 render(){
     return(
-        <div>
-            <img alt="biscoito-da-sorte" src={require('./assets/biscoito.png')}/>
+        <div className='container'>
+            <img className='img' alt="biscoito-da-sorte" src={require('./assets/biscoito.png')}/>
+            <Botao/>
+            <h3 className='textoFrase'>Frase número 1 aleatória</h3>
         </div>
     )
 }
 }
 
 export default App;
+
+class Botao extends Component{
+    render(){
+        return(
+            <div>
+                <button>Abrir biscoito</button>
+            </div>
+        )
+    }
+}
